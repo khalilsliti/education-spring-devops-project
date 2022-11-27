071b64b98f96fb1ff52c70e21b07e6d6658a842e
khalilsliti <slitikhalil1998@gmail.com>
2022-11-27 16:47:57 +0100

    university service finished

---
+++ src/main/java/com/project/education/repositories/UniversityRepository.java
@@ -0,0 +1,8 @@
+package com.project.education.repositories;
+
+import com.project.education.entities.University;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UniversityRepository extends JpaRepository<University,Integer> {
+
+}
