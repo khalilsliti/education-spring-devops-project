932fe0c09927f0f565886bd25cdd0e5ae774fe78
khalilsliti <slitikhalil1998@gmail.com>
2022-12-09 10:56:33 +0100

    testing webhook with ngrok

--- script.groovy
+++ script.groovy
@@ -27,7 +27,6 @@ def sonarTest() {
 def cleanUntaggedImages() {
     def cleanImages = 'docker image prune --force --filter "dangling=true"'
     sshagent(credentials: ['jenkins-server']) {
-
         sh "${cleanImages}"
     }
 }
